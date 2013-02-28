c9cfe38b311fb54ff47f0834240e2d4595e4225d
skalinets <kalinets@gmail.com>
2013-02-28 23:09:15 +0200

    added default application

---
+++ src/Site/ConfigureFubuMVC.cs
@@ -0,0 +1,13 @@
+using FubuMVC.Core;
+
+namespace Site
+{
+    public class ConfigureFubuMVC : FubuRegistry
+    {
+        public ConfigureFubuMVC()
+        {
+            // As is, this will be using all the default conventions and policies
+
+        }
+    }
+}
